94dbf9a34645be28ccfbe536499d4b04bde04589
Justin <justinch@uw.edu>
2024-11-18 20:44:52 -0800

    Created token. TODO, get functionality for resetting password

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from fastapi import FastAPI
-
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from contextlib import asynccontextmanager
-import bcrypt
 
-class User(SQLModel, table=True):
+import bcrypt
+from utils import (
+    get_hashed_password,
+    create_access_token,
+    verify_password
+)
+
+class UserBase(SQLModel):
     username: str = Field(index=True, primary_key=True)
     hashed_password: str = Field(index=True) 
+
+class User(UserBase, table=True):
     email: str = Field(index=True)
+
+
+class TokenSchema():
+    access_token: str
+    expiration: str
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -40,20 +51,42 @@ async def running():
     return {"message": "Hello World!"}
 
 # Generates a JWT token and the valid amount of time this is valid for
-@app.post("/validate/")
-async def generate_token(username, password):
-    return {"message": "Finished"}
+@app.post('/validate', summary="Create access and refresh tokens for user")
+async def login(session: SessionDep, user: UserBase):
+    databaseQuery = session.get(User, user.username)
 
-@app.post("/users/")
+    if databaseQuery is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Incorrect email or password"
+        )
+
+    existingPassword = databaseQuery.hashed_password
+    providedPassword = user.hashed_password
+
+    if not verify_password(providedPassword, existingPassword):
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect email or password"
+        )
+
+    return {
+        "token": create_access_token("Runnerup", 30),
+        "
+    }
+
+
+@app.post("/signup")
 def create_user(user: User, session: SessionDep) -> User:
-    unhashed = user.hashed_password
 
-    bytes = unhashed.encode('utf-8')
-    salt = bcrypt.gensalt()
+    check = session.get(User, user.username)
 
-    hashed = bcrypt.hashpw(bytes, salt)
+    if check is not None:
+        raise HTTPException(
+            status_code=400, detail="User with this email already exists!"
+        )
 
-    user.hashed_password = hashed
+    user.hashed_password = get_hashed_password(user.hashed_password)
 
     session.add(user)
     session.commit()
@@ -76,6 +109,7 @@ def delete_user(username: str, session: SessionDep):
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
+    
     session.delete(user)
     session.commit()
     return {"deleted": "true"} 
